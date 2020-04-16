872b8444608c811457a49349397148435c6370b8
Daniel Cooper <cooperdaniel5141@gmail.com>
2020-04-16 07:57:36 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,9 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  names = "Hello #{player1} & #{playar2}"
-  return names
+  "Hello #{player1} & #{playar2}"  
 end
+start_game
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
